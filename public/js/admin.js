7af3550b7ec8561bd8afe8b4648a2072c1302975
Jonathan <jonathannoahtadese@gmail.com>
2020-02-17 13:51:11 +0100

    Fixa infopanel (timer och text)

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,32 +1,43 @@
- //document.getElementById('timer').value;
 
-function timer() {
-    console.log("hello")
-    //console.log(time)
-
-    var countDown = setInterval(function() {
-	console.log("yo")
-	var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-	var now = new Date().getTime();
-	var diff = countDownDate - now;
-
-	
-	// Time calculations for days, hours, minutes and seconds
-	var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-	// Display the result in the element with id="timer"
-	document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-
-	// If the count down is finished, write some text 
-	if (minutes < 1 && seconds == 0) {
-	    clearInterval(countDown);
-	    document.getElementById("timer").innerHTML = "Meeting is over";
-	}
-    }, 1000);
-    
-
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+async function timer() {
+    //choosen time in seconds
+    let time = (document.getElementById('time').value) * 60;
+    let diff = time;
+
+    if(diff > 0){
+	while(diff > 0){
+	    let minutes = Math.floor(diff / 60);
+	    let seconds = diff % 60;
+
+	    // Give page time to print
+	    await sleep(1000);
+	    
+	    // Display the result in the element with id="timer"
+	    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+
+	    // Decrement difference 
+	    diff--;
+
+	    // If the count down is finished, write some text 
+	    if (diff == 0) {
+		document.getElementById("timer").innerHTML = "Mötet är över";
+		await sleep(4000);
+		document.getElementById("timer").innerHTML = "";
+		document.getElementById("time").innerHTML = "Längd på event";
+	    }
+	}
+    } else {
+	document.getElementById("timer").innerHTML = "Vänligen ge ett giltigt värde (värde över 0)";
+    }
+}
+
+
+
 
 
 
