ceae9977d75bb6a300653e6a996afe1fdfc6db87
Jonathan <jonathannoahtadese@gmail.com>
2020-02-20 16:20:02 +0100

    Fixed so timer could not be changed in middle of running

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,39 +1,45 @@
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
+let timerOn = false;
 async function timer() {
-	//choosen time in seconds
-	let time = (document.getElementById('time').value) * 60;
-	let diff = time;
+    //choosen time in seconds
+    let time = (document.getElementById('time').value) * 60;
+    let diff = time;
 
-	if (diff > 0) {
-		while (diff > 0) {
-			let minutes = Math.floor(diff / 60);
-			let seconds = diff % 60;
+    if (diff > 0 && !timerOn) {
+	timerOn = true;
+	
+	while (diff > 0) {
+	    let minutes = Math.floor(diff / 60);
+	    let seconds = diff % 60;
 
-			// Display the result in the element with id="timer"
-			document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+	    // Display the result in the element with id="timer"
+	    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
-			// Give page time to print
-			await sleep(1000);
+	    // Give page time to print
+	    await sleep(1000);
 
-			// Decrement difference 
-			diff--;
+	    // Decrement difference 
+	    diff--;
 
-			// If the count down is finished, write some text 
-			if (diff == 0) {
-				document.getElementById("timer").innerHTML = "Mötet är över";
-				await sleep(4000);
-				document.getElementById("timer").innerHTML = "";
-				document.getElementById("time").innerHTML = "Längd på event";
-			}
-		}
-	} else {
-		document.getElementById("timer").innerHTML = "Vänligen ge ett giltigt värde (värde över 0)";
+	    // If the count down is finished, write some text 
+	    if (diff == 0) {
+		document.getElementById("timer").innerHTML = "Mötet är över";
+		await sleep(4000);
+		document.getElementById("timer").innerHTML = "";
+		document.getElementById("time").innerHTML = "Längd på event";
+		timerOn = false;
+	    }
 	}
+    } else if(timerOn){
+	document.getElementById("timer").innerHTML = "Vänligen vänta tills mötet är över";	
+	await sleep(20000);
+    } else {
+	document.getElementById("timer").innerHTML = "Vänligen ge ett giltigt värde (värde över 0)";
+    }
 }
 
 if (!Modernizr.draganddrop)
