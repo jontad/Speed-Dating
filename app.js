72c434ae327be390b7cc3d184b6e2172ef8a74b0
Jonathan <jonathannoahtadese@gmail.com>
2020-03-16 14:32:33 +0100

    sharedContacts

--- app.js
+++ app.js
@@ -220,6 +220,10 @@ io.on('connection', function (socket) {
     socket.on('getMatches', function (matches) {
         io.emit('currentMatches', { matches: data.getMatches() });
     });
+    
+     socket.on('makeContactWith', function (contacts) {
+        io.emit('currentMatches', { matches: contacts.getMatches() });
+    });
 });
 
 /* eslint-disable-next-line no-unused-vars */
