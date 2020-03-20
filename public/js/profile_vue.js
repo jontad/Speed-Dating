1705deff12e1c1b22f80ff80fd2a4cb56c24e115
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2020-03-20 11:23:34 +0100

    Fixed dummys, fixed minor estethics

--- public/js/profile_vue.js
+++ public/js/profile_vue.js
@@ -52,83 +52,6 @@ let dummyContacts = [dateDummy1, dateDummy2,dateDummy3];
 
 let meetingUser = null;
 
-// DUMMY CODE
-let dummyUsers = [
-    {
-        name: "a",
-        age: 1,
-        description: "aa",
-        location: "hejsan",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "a@a.a",
-        gender: "male",
-        password: "a",
-        userName: "a"
-    },
-    {
-        name: "B",
-        age: 1,
-        description: "bb",
-        location: "hejsan bbb",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "b@b.b",
-        gender: "female",
-        password: "b",
-        userName: "b"
-    },
-    {
-        name: "c",
-        age: 1,
-        description: "cc",
-        location: "hejsan",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "c@c.c",
-        gender: "male",
-        password: "c",
-        userName: "c"
-    },
-    {
-        name: "D",
-        age: 1,
-        description: "dd",
-        location: "hejsan ddd",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "d@d.d",
-        gender: "female",
-        password: "d",
-        userName: "d"
-    },
-    {
-        name: "e",
-        age: 1,
-        description: "ee",
-        location: "hejsan eee",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "e@e.e",
-        gender: "male",
-        password: "e",
-        userName: "e"
-    },
-    {
-        name: "F",
-        age: 1,
-        description: "FF",
-        location: "loaction FF",
-        picture: "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg",
-        phoneNumber: "11111111111",
-        email: "f@f.f",
-        gender: "female",
-        password: "f",
-        userName: "f"
-    }
-];
-
-
 
 const vm = new Vue({
     el: 'main',
@@ -166,7 +89,6 @@ const vm = new Vue({
         afterDateAnswers: [0, 0, 0, 0],
         other: '',
 
-	      dummyUsers: dummyUsers,
 
     },
     mounted() {
@@ -282,7 +204,7 @@ const vm = new Vue({
         },
         addDefaultPicture: function () {
             if (!this.picture) {
-                this.picture = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                this.picture = "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg";
             }
         },
         login: function () {
@@ -407,30 +329,5 @@ const vm = new Vue({
             this.checkMatches();
             window.location.href="/user-contacts";
         },
-	      dummys: function () {
-	          let newUser;
-
-	          for (var i = 0; i < dummyUsers.length; i++) {
-		            let name = dummyUsers[i].name;
-		            let age = dummyUsers[i].age;
-		            let description = dummyUsers[i].description;
-		            let address = dummyUsers[i].address;
-		            let picture = dummyUsers[i].picture;
-		            let number = dummyUsers[i].phoneNumber;
-		            let mail = dummyUsers[i].email;
-		            let password = dummyUsers[i].password;
-		            let userName = dummyUsers[i].userName;
-		            let gender = dummyUsers[i].gender;
-		            
-		            newUser = new Profile(name, age, description,
-					                            address, picture,
-					                            number, mail,
-					                            password, userName, gender);
-
-		            console.log(newUser)
-		            socket.emit('addNewUser', newUser);
-	          }
-	          this.currentUser = newUser;
-	      },
     }
 });
